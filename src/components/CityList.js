01c0f25a21776d3c8d58fde9aa82c639c4d8230b
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2023-09-02 10:23:21 +0530

    updating css

--- src/components/CityList.js
+++ src/components/CityList.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import WeatherService from "../services/wetherservice";
 import cityData from "../data/cities.json";
-import { Weather } from "./Weather";
+import  Weather  from "./Weather";
 import "./MainWeather.css";
 import WbSunnyIcon from "@mui/icons-material/WbSunny";
 import Headerbg from "./Headerbg.png";
@@ -46,6 +46,13 @@ const CityList = () => {
     }
   };
 
+  // Split the weatherData into pairs for each row
+  const weatherPairs = [];
+  for (let i = 0; i < weatherData.length; i += 2) {
+    const pair = weatherData.slice(i, i + 2);
+    weatherPairs.push(pair);
+  }
+
   return (
     <div
       style={{
@@ -56,48 +63,53 @@ const CityList = () => {
       }}
     >
       <div>
-        <p style={{ fontSize: "20px", position: "relative", top: "30px" }}>
+        <p style={{ fontSize: "20px", position: "relative" }}>
           <b>
             <center>
               <WbSunnyIcon /> Weather App
             </center>
           </b>
         </p>
-        <Row>
-          <Col lg={6}>
-            {weatherData.map((city, index) => (
-              <Weather
-                key={index}
-                city={city.list[0].name}
-                temp={city.list[0].main.temp}
-                date={new Date(city.list[0].dt * 1000).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: true,
-                })}
-                desc={city.list[0].weather[0].description}
-                color={getColorForDescription(city.list[0].weather[0].description)}
-                pressure={city.list[0].main.pressure}
-                humidity={city.list[0].main.humidity}
-                visibility={city.list[0].visibility / 1000}
-                temp_min={city.list[0].main.temp_min}
-                temp_max={city.list[0].main.temp_max}
-                windspeed={city.list[0].wind.speed}
-                degree={city.list[0].wind.deg}
-                sunrise={new Date(city.list[0].sys.sunrise * 1000).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: true,
-                })}
-                sunset={new Date(city.list[0].sys.sunset * 1000).toLocaleTimeString([], {
-                  hour: "2-digit",
-                  minute: "2-digit",
-                  hour12: true,
-                })}
-              />
-            ))}
-          </Col>
-        </Row>
+        <div>
+          {weatherPairs.map((pair, index) => (
+            <Row key={index} className="weather-row">
+              {pair.map((city, cityIndex) => (
+                <Col lg={6} key={cityIndex} style={{}}>
+                  <Weather style={{ color: "white" }}
+                    key={cityIndex}
+                    city={city.list[0].name}
+                    temp={city.list[0].main.temp}
+                    date={new Date(city.list[0].dt * 1000).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                    desc={city.list[0].weather[0].description}
+                    color={getColorForDescription(city.list[0].weather[0].description)}
+                    pressure={city.list[0].main.pressure}
+                    humidity={city.list[0].main.humidity}
+                    visibility={city.list[0].visibility / 1000}
+                    temp_min={city.list[0].main.temp_min}
+                    temp_max={city.list[0].main.temp_max}
+                    windspeed={city.list[0].wind.speed}
+                    degree={city.list[0].wind.deg}
+                    sunrise={new Date(city.list[0].sys.sunrise * 1000).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                    sunset={new Date(city.list[0].sys.sunset * 1000).toLocaleTimeString([], {
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      hour12: true,
+                    })}
+                    
+                  />
+                </Col>
+              ))}
+            </Row>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,91 +1,103 @@
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import NearMeOutlinedIcon from "@mui/icons-material/NearMeOutlined";
+import Clooounds from "./cloounds.png";
+import CloudQueueOutlinedIcon from "@mui/icons-material/CloudQueueOutlined";
+import "./Weather.css"; // Import the CSS file for styling
 
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia'; 
-import NearMeOutlinedIcon from '@mui/icons-material/NearMeOutlined';
-import Clooounds from "./cloounds.png"
-import { Container } from 'react-bootstrap';
-import CloudQueueOutlinedIcon from '@mui/icons-material/CloudQueueOutlined';
-const colo = "red";
-export const Weather = (props) => {
-  const {city,temp,date,desc,pressure,humidity,visibility,temp_min,temp_max,windspeed,degree,sunrise,sunset,color} = props;
+const Weather = (props) => {
+  const {
+    city,
+    temp,
+    date,
+    desc,
+    pressure,
+    humidity,
+    visibility,
+    temp_min,
+    temp_max,
+    windspeed,
+    degree,
+    sunrise,
+    sunset,
+    color,
+  } = props;
+
   return (
-
-   
-
-<div style={{marginTop:"50px",marginLeft:"20px"}}>
-    <Card sx={{ maxWidth: 500 }}>
-      <CardMedia style={{backgroundColor:color,height:"250px"}}
-        sx={{ height: 140 }}
-        image  ={Clooounds}
-        title="green iguana"
-        
-      >
-        <Container fluid style={{padding:"20px",color:"white",fontFamily:"inter"}}>
-        <Row  >
-            <Col >
-            <p style={{fontSize:"30px"}}><b>{city} </b></p>
-            <p>{date}</p>
-            </Col>
-            <Col>
-           <p style={{fontSize:"50px"}}> <b>{temp}&deg;C </b></p> 
-            </Col>
-        </Row>
-        <Row >
-            <Col style={{padding:"20px 0px"}}>
-           <p > <CloudQueueOutlinedIcon/> <b> {desc} </b> </p>  
-            </Col>
-            <Col>
-            <p><b>Temp min : {temp_min}&deg;C</b></p>
-            <p> <b>Temp max : {temp_max}&deg;C</b></p>
-            </Col>
-        </Row>
-        </Container>
+    <div className="weather-container">
+      <Card sx={{ maxWidth: 600 }}>
+        <CardMedia
+          sx={{
+            height: 250,
+            backgroundColor: color,
+          }}
+          image={Clooounds}
+          title="green iguana"
+        >
+          <CardContent>
+            <div className="weather-header">
+              <div className="weather-header-left">
+                <p className="city">{city}</p>
+                <p className="date">{date}</p>
+              </div>
+              <div className="weather-header-right">
+                <p className="temperature">{temp}&deg;C</p>
+              </div>
+            </div>
+            <div className="weather-info">
+              <div className="weather-icon">
+                <CloudQueueOutlinedIcon />
+                <b>{desc}</b>
+              </div>
+              <div className="temperature-range">
+                <p>
+                  <b>Temp min: {temp_min}&deg;C</b>
+                </p>
+                <p>
+                  <b>Temp max: {temp_max}&deg;C</b>
+                </p>
+              </div>
+            </div>
+          </CardContent>
         </CardMedia>
 
-      <CardContent style={{color:"white" , backgroundColor:"#383b47",fontFamily:"inter"}}>
-<Row>
-
-<Col>
-<p><b>Presure: </b> {pressure} hpa</p>
-<p><b>Humidity:</b> {humidity} %</p>
-<p><b>Visibility:</b> {visibility} km</p>
-</Col>
-<hr style={{width:"10px",height:"100px"}}></hr>
-<Col style={{padding:"12px"}}>
-<center><NearMeOutlinedIcon/></center>
-<center><p>{windspeed} m/s { degree} degree</p></center>
-</Col>
-
-
-<Col>
-<p><b>Sunrise:</b> {sunrise}</p>
-<p><b>Sunset:</b> {sunset}</p>
-</Col>
-
-{/* #6249cc - broken -purple
-#40b681 - clear sky - green
-#de944e - light rain - orange
-#9c3a3a - heavy rain - red */}
-
-        {/* <Typography gutterBottom variant="h5" component="div">
-          Lizard
-        </Typography> */}
-        {/* <Typography variant="body2" color="text.secondary">
-          Lizards are a widespread group of squamate reptiles, with over 6,000
-          species, ranging across all continents except Antarctica
-        </Typography> */}
-        </Row>
-      </CardContent>
-      {/* <CardActions>
-        <Button size="small">Share</Button>
-        <Button size="small">Learn More</Button>
-      </CardActions> */}
-    </Card>
-
+        <CardContent className="weather-details">
+          <div className="weather-details-left">
+            <p>
+              <b>Pressure:</b> {pressure} hpa
+            </p>
+            <p>
+              <b>Humidity:</b> {humidity} %
+            </p>
+            <p>
+              <b>Visibility:</b> {visibility} km
+            </p>
+          </div>
+          <hr className="weather-details-divider" />
+          <div className="weather-details-center">
+            <center>
+              <NearMeOutlinedIcon />
+            </center>
+            <center>
+              <p>
+                {windspeed} m/s {degree} degree
+              </p>
+            </center>
+          </div>
+          <div className="weather-details-right">
+            <p>
+              <b>Sunrise:</b> {sunrise}
+            </p>
+            <p>
+              <b>Sunset:</b> {sunset}
+            </p>
+          </div>
+        </CardContent>
+      </Card>
     </div>
   );
-}
+};
+
+export default Weather;
