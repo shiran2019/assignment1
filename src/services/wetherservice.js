2f049f1ead4df9601256f56be688cc5192977ab5
Shiran Maduwantha <88098649+shiran2019@users.noreply.github.com>
2023-09-07 10:58:13 +0530

    remove unnessary coments

--- src/services/wetherservice.js
+++ src/services/wetherservice.js
@@ -1,56 +1,6 @@
-// import axios from "axios";
-
-// const WeatherService = {
-//   getWeatherByCityCode: async (cityCode) => {
-//     try {
-//       console.log("hi");
-//       const response = await axios.get(
-//         `https://api.openweathermap.org/data/2.5/group?id=${cityCode}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
-//       );
-//       console.log(response.data);
-//       return response.data;
-//     } catch (error) {
-//       console.error("Error fetching weather data:", error);
-//       throw error;
-//     }
-//   },
-// };
-// export default WeatherService;
-
-
-
-
-
-// import axios from "axios";
-
-// const WeatherService = {
-//   getWeatherByCityCode: async (cityCode) => {
-//     try {
-//       const response = await axios.get(
-//         `https://api.openweathermap.org/data/2.5/group?id=${cityCode}&units=metric&appid=${process.env.REACT_APP_WEATHER_KEY}`
-//       );
-
-//       // Wrap each item in the 'list' array with a 'cnt' property set to 1
-//       const modifiedData = response.data.list.map((item) => ({
-//         cnt: 1,
-//         list: [item],
-//       }));
-
-//       return modifiedData;
-//     } catch (error) {
-//       console.error("Error fetching weather data:", error);
-//       throw error;
-//     }
-//   },
-// };
-
-// export default WeatherService;
-
-
-
-
 import axios from "axios";
 import { CACHE_EXPIRATION } from "../constants/Constant";
+
 
 const WeatherService = {
   getWeatherByCityCode: async (cityCode) => {
@@ -62,11 +12,11 @@ const WeatherService = {
 
 
         console.log("Returning cached data");
-    console.log(data);
+        console.log(data);
 
         const eData = data.list.map((item) => ({
-          cnt: 1,
-          list: [item],
+        cnt: 1,
+        list: [item],
         }));
 
         console.log(eData);
